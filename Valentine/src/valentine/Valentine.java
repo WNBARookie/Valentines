010dbe4f0032c8caa9dbffc51d34777221a21d4e
ConnorButton <34140378+ConnorButton@users.noreply.github.com>
2017-11-30 13:54:34 -0600

    Sorting Method
    Maybe this time?

---
+++ Valentine/src/valentine/Valentine.java
@@ -0,0 +1,10 @@
+package valentine;
+public class Valentine {
+
+    public static void main(String[] args) {}
+    
+    private String[][] Sort(String[][] Input){
+    
+        return Input;
+    }
+}
